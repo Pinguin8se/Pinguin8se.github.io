8a573727820ebaf4d20c4bf21b9c2f434677c0f2
Pinguin8se <118835348+Pinguin8se@users.noreply.github.com>
2022-12-09 04:08:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('💗Si sabia que me amabas, no hacia falta que me lo demuestres💗');
+    alert('💗Si sabia que me amabas, no hacia falta que me lo demuestres.💗');
 });
 
 const noBtn = document.querySelector('#noBtn');
