307818f4ebd4d719a9538a0d8c911c5e71543e3a
Pinguin8se <118835348+Pinguin8se@users.noreply.github.com>
2024-02-04 00:22:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Ya lo sabia, muy mal mija, que decepcion');
+    alert('Ya lo sabia,te amo mucho❤️');
 });
 
 const noBtn = document.querySelector('#noBtn');
