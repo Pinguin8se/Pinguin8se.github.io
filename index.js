2c74d4d24254d3bc01e2a2e6550f38cb0c3fb795
Pinguin8se <118835348+Pinguin8se@users.noreply.github.com>
2026-01-30 21:27:05 -0500

    Change alert message for yes button
    Updated alert message for yes button click event.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('ayy te amito un chingo Aitana❤️,gracias x llegar a mi vida y alegrarmela ademas te doy para ti todos estos tulipanes🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷🌷');
+    alert('cacorra');
 });
 
 const noBtn = document.querySelector('#noBtn');
@@ -13,3 +13,4 @@ noBtn.addEventListener('mouseover', function () {
     noBtn.style.setProperty('left',randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
 })
+
